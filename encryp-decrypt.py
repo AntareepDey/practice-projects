dbd00c15edf69f21d2dc427f512be3e0cc5c2b94
AntareepDey <antareep175@hotmail.com>
2021-03-19 00:13:18 +0530

    version1.0

--- encryp-decrypt.py
+++ encryp-decrypt.py
@@ -1,1 +1,29 @@
-sen=input()
+alphabet='abcdefghijklmnopqrstuvwxyz'
+key=5
+
+def encryption(x):
+        encrypt=''
+        for i in x:
+                p=alphabet.find(i)
+                np=(p+key)% 26
+                encrypt+=alphabet[np]
+        return encrypt
+
+def decryption(y):
+        decrypt=''
+        for i in y:
+                p=alphabet.find(i)
+                np=(p-key)%26
+                decrypt+=alphabet[np]
+        return decrypt
+
+print("What do you Want to do :")
+print("1.Encrypt the code")
+print("2.Decrypt the code")
+choice=int(input("What is your choice:"))
+if choice==1:
+        msg=input("Enter the word:")
+        print("The encrypted code:",encryption(msg))
+elif choice==2:
+        msg=input("Enter Encrypted code:")
+        print("The decrypted code:",decryption(msg))  
